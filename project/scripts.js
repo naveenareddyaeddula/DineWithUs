ec03d1a859966a3cabdad8c8a3aeef1cd739d710
NaveenaAeddula <naveena@fyers.in>
2023-09-01 11:50:56 +0530

    add: 1. index page creation, 2. menu & about details updation

---
+++ project/scripts.js
@@ -0,0 +1,18 @@
+function dap() {
+    alert("Your item added to order Successfully");
+}
+function dapFinal(){
+	alert("Your order placed Successfully");
+}
+
+function linkedin() {
+    window.location.href = 'https://www.linkedin.com/in/archana-patukuri-495055162/';
+}
+
+function facebook() {
+    window.location.href = 'https://www.facebook.com/archana.patukuri.9';
+}
+
+function instagram() {
+    window.location.href = 'https://www.instagram.com/archana_patukuri/';
+}
